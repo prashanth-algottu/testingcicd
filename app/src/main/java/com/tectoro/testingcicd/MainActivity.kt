1f04eee81c6f5be6ba1e4cc0b21be96d727c083c
Prashanth <prashanth.algottu@tectoro.com>
2025-03-12 15:35:46 +0530

    changed package name

--- app/src/main/java/com/tectoro/testingcicd/MainActivity.kt
+++ app/src/main/java/com/tectoro/testingcicd/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.tectoro.testingcicd
-
+//com.tectoro.buildtesting
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tectoro.testingcicd.ui.theme.TestingcicdTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +37,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Version name ${getAppVersion(LocalContext.current)}",
+            modifier = modifier.padding(top = 60.dp)
+        )
+        Text(text = "Package name : ${LocalContext.current.packageName}")
+    }
+
+}
+
+private fun getAppVersion(context: Context): String {
+    val packageManager = context.packageManager
+    val packageName = context.packageName
+    val versionName = packageManager.getPackageInfo(packageName, 0).versionName
+    return "Version: $versionName"
 }
 
 @Preview(showBackground = true)
